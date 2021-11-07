2c714b5a0ab733e7952c2f1016f69835cf71638b
ismaeldev04 <ismaeldev04@gamil.com>
2021-11-07 14:40:53 -0400

    class3
    lo q hicimos en esta clase fue implementar al dinosaurio sus distintas imagenes en la respectivas posiciones x e y  y sus respectivas acciones las cuales son : saltar(jump) , agacharse(duck) y correr (run) .

--- nlc_dino_runner/componers/game.py
+++ nlc_dino_runner/componers/game.py
@@ -1,4 +1,5 @@
 import pygame
+
 from nlc_dino_runner.utils.constants import (
     TITTLE,
     ICON,
@@ -7,19 +8,21 @@ from nlc_dino_runner.utils.constants import (
     BG
 
 )
+from nlc_dino_runner.componers.dinosaurio import Dinosaur
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.playing = False
         pygame.display.set_caption(TITTLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.x_pos_bg = 0
-        self.y_pos_bg = 400
-        self.game_speed = 20
         self.clock = pygame.time.Clock()
+        self.playing = False
+        self.game_speed = 20
+        self.x_pos_bg = 0
+        self.y_pos_bg = 380
+        self.player = Dinosaur()
 
     def run(self):
         self.playing = True
@@ -29,19 +32,21 @@ class Game:
             self.draw()
         pygame.quit()
 
-
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(30)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
+        pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
